46a673664f6c48298d005712e2948ead7f78d0e1
qkrdydrbs <114204582+qkrdydrbs@users.noreply.github.com>
2022-10-06 21:29:59 +0900

    Add files via upload

---
+++ Report_1st,.py
@@ -0,0 +1,33 @@
+print("환영합니다."
+      "파이썬의 세계에 오신 것을 환영합니다."
+      "파이썬은 강력합니다.")
+환영합니다.파이썬의 세계에 오신 것을 환영합니다.파이썬은 강력합니다.
+2.
+2.0
+print("반갑습니다. 파이썬!")
+반갑습니다. 파이썬!
+print(2*3/10)
+0.6
+print("hello", "world", "!!!")
+hello world !!!
+
+
+3
+3
+print(7*24)
+168
+
+
+4
+4
+import turtle
+turtle.shape("turtle")
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
